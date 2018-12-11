67cd48abc867d927faa1f35e67fa94081725d154
ju-zp <jhuntington@tutanota.com>
2018-12-11 12:39:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ControllerInput from './components/ControlledInput'
+
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControllerInput/>,
   document.getElementById('root')
 );
